4fff46a71612d154a083f17d89b7c9d053b8513d
chrismarspink <jkkim7202@gmail.com>
2021-11-10 12:49:32 +0900

    add cms_xxx api

--- webserver.py
+++
@@ -1,9 +0,0 @@
-import http.server
-import sockerserver
-
-handler = http.server.SimpleHTTPRequestHandler
-
-with sockerserver.TCPServer((''m 8001), handler) as httpd:
-    print('server Listening on port 8001...')
-    httpd.serve_forever()
-    
